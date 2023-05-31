14be763e85122ae287df252906f68d09cbd12369
Christiaan <97374859+ChristiaanSmall@users.noreply.github.com>
2023-05-31 15:18:06 +1000

    upade

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 // Example code to test JavaScript APIs
 console.log('Testing JavaScript APIs');
+src="//static.klaviyo.com/onsite/js/klaviyo.js?company_id=PUBLIC_API_KEY"
+klaviyo.push(['identify', {
+    // Change the line below to dynamically print the user's email.
+    '$email' : 'abc@gmail.com'
+  }]);
